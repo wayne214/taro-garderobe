3b9c416a4b73bfb1880b1574ac97e5dda71dd566
wayne <jun_wei_like1@163.com>
2018-12-19 17:37:55 +0800

    路由跳转

--- src/pages/index2/index.js
+++ src/pages/index2/index.js
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import './index.scss'
 
 export default class Index extends Component {
   config = {
